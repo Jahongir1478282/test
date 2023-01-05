9ef77b2653734d7b41f21f3a8b5e6e7e191e594f
Jahongir1478282 <jahongir36972@gmail.com>
2023-01-05 19:54:11 +0500

    Studentning birinchi commiti

---
+++ components/Student.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Student Component</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+      omnis distinctio rem cumque officia tempora itaque veniam,
+      recusandae, voluptatem doloribus odit dicta dolores autem
+      fugiat, fuga excepturi impedit laudantium. Nulla.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
